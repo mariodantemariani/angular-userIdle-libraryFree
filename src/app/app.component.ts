8e4d8332774b8719aee9fcbd8b4d837054d17cae
mario.mariani <mariodante@gmail.com>
2023-11-14 20:13:02 -0300

    completed code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { IdleUserService } from './services/idle-service.service';
+
+const SERVICES: any[] = [IdleUserService];
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
+  providers: [SERVICES],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+  title = 'Idle User Service Example';
+
+  isUserIdle: boolean = false;
+
+  constructor(private idleUserService: IdleUserService) {}
+
+  ngOnInit() {
+    this.idleUserService.userInactive.subscribe(
+      (isIdle) => (this.isUserIdle = isIdle)
+    );
+  }
+
+  reset() {
+    console.log('Reset idle timer');
+    this.isUserIdle = false;
+    this.idleUserService.reset();
+  }
 }
---
+++ src/app/services/idle-service.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { IdleUserTimes } from './app.data';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IdleUserService {
+  private timeoutId: any;
+  private countdownId: any;
+  private countdownValue!: number;
+
+  userInactive: Subject<boolean> = new Subject();
+  constructor() {
+    this.reset();
+    this.initListener();
+  }
+
+  initListener() {
+    window.addEventListener('mousemove', () => this.reset());
+    window.addEventListener('click', () => this.reset());
+    window.addEventListener('keypress', () => this.reset());
+    window.addEventListener('DOMMouseScroll', () => this.reset());
+    window.addEventListener('mousewheel', () => this.reset());
+    window.addEventListener('touchmove', () => this.reset());
+    window.addEventListener('MSPointerMove', () => this.reset());
+  }
+
+  reset() {
+    clearTimeout(this.timeoutId);
+    clearTimeout(this.countdownId);
+    this.startIdleTimer();
+  }
+
+  startIdleTimer() {
+    this.timeoutId = setTimeout(() => {
+      console.log('Inactivity detected');
+      this.startCountdown();
+    }, IdleUserTimes.IdleTime);
+  }
+
+  startCountdown() {
+    this.countdownValue = IdleUserTimes.CountdownTime / 1000;
+    this.countdownId = setInterval(() => {
+      this.countdownValue--;
+      console.log('You will logout in:', this.countdownValue, 'seconds');
+      if (this.countdownValue <= 0) {
+        clearInterval(this.countdownId);
+        console.log('User idle');
+        this.userInactive.next(true);
+      }
+    }, 1000);
+  }
+}
